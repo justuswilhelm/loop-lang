4b710346725fbf6cf626d52f5a5a8e3f837ee326
Justus Perlwitz <mail@justusperlwitz.com>
2015-12-16 00:14:48 +0100

    Switch to parsimonius for parsing

--- __init__.py
+++ __init__.py
@@ -1,50 +1,17 @@
-"""
-P := xi := xj +c
-    | xi := xj - c
-    | P; P
-    | LOOP xi DO P END
-"""
-from sys import stdin
+from parsimonious.grammar import Grammar
 
-from tdparser import (
-    Token,
-    Lexer,
-)
+grammar = Grammar(r"""
+program = (assignment / loop / output)*
 
+NAME = ~r"x\d+"
+NUMBER = ~r"(?:[1-9]\d*)|0"
+MINUS = "-"
+PLUS = "+"
+ASSIGNMENT = ":="
+SEMICOLON = ";"
+_ = ~"\s*"
 
-class Variable(Token):
-    regexp = r'x\d+'
-
-
-class Constant(Token):
-    regexp = r'\d+'
-
-
-class Plus(Token):
-    regexp = '\+'
-
-
-class Assignment(Token):
-    regexp = r':='
-
-
-class StatementEnd(Token):
-    regexp = r';'
-
-
-class NewLine(Token):
-    regexp = r'\n'
-
-
-lexer = Lexer()
-lexer.blank_chars = ' \n'
-lexer.register_token(Assignment)
-lexer.register_token(Constant)
-lexer.register_token(Plus)
-lexer.register_token(StatementEnd)
-lexer.register_token(Variable)
-lexer.register_token(NewLine)
-
-if __name__ == "__main__":
-    inp = stdin.read()
-    print(inp)
+assignment = NAME _ ASSIGNMENT _ (NAME _ ( "+" / "-" ) _)? NUMBER _ SEMICOLON _
+output = NAME _ SEMICOLON _
+loop = "LOOP" _ NAME _ "DO" _ program _ "END" _
+""")

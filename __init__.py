79b8c8758797334acdbee936a5c864379e10140d
Justus Perlwitz <mail@justusperlwitz.com>
2015-12-17 00:30:17 +0100

    Add AST traverser

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-from parsimonious.grammar import Grammar
+from parsimonious.grammar import (
+    Grammar,
+    NodeVisitor,
+)
 
 grammar = Grammar(r"""
 program = (assignment / loop / output)*
@@ -9,9 +12,62 @@ MINUS = "-"
 PLUS = "+"
 ASSIGNMENT = ":="
 SEMICOLON = ";"
-_ = ~"\s*"
+WHITESPACE = ~r"\s*"
+LOOP_SETUP = "LOOP"
+LOOP_START = "DO"
+LOOP_END = "END"
 
-assignment = NAME _ ASSIGNMENT _ (NAME _ ( "+" / "-" ) _)? NUMBER _ SEMICOLON _
-output = NAME _ SEMICOLON _
-loop = "LOOP" _ NAME _ "DO" _ program _ "END" _
+assignment = NAME WHITESPACE ASSIGNMENT WHITESPACE
+    (NAME WHITESPACE ( PLUS / MINUS ) WHITESPACE)?
+    NUMBER WHITESPACE SEMICOLON WHITESPACE
+output = NAME WHITESPACE SEMICOLON WHITESPACE
+loop = LOOP_SETUP WHITESPACE NAME WHITESPACE LOOP_START WHITESPACE program
+    WHITESPACE LOOP_END WHITESPACE
 """)
+
+
+class Visitor(NodeVisitor):
+    def visit_NAME(self, node, visit):
+        ...
+
+    def visit_(self, node, visit):
+        pass
+
+    def visit_ASSIGNMENT(self, node, visit):
+        ...
+
+    def visit_WHITESPACE(self, node, visit):
+        pass
+
+    def visit_NUMBER(self, node, visit):
+        ...
+
+    def visit_SEMICOLON(self, node, visit):
+        pass
+
+    def visit_assignment(self, node, visit):
+        ...
+
+    def visit_PLUS(self, node, visit):
+        ...
+
+    def visit_MINUS(self, node, visit):
+        ...
+
+    def visit_LOOP_SETUP(self, node, visit):
+        ...
+
+    def visit_LOOP_START(self, node, visit):
+        ...
+
+    def visit_program(self, node, visit):
+        ...
+
+    def visit_LOOP_END(self, node, visit):
+        ...
+
+    def visit_loop(self, node, visit):
+        ...
+
+    def visit_output(self, node, visit):
+        ...
